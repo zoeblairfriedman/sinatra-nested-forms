9a425e8025d12106f108c4da1e7e67556bd5479c
Zoe Friedman <zoeblair.friedman@gmail.com>
2021-02-24 09:10:21 -0500

    commit tree f6e93c8420a9b208ef19c008df4925e371cc4b2f

--- app.rb
+++ app.rb
@@ -3,7 +3,22 @@ require './environment'
 module FormsLab
   class App < Sinatra::Base
 
-    # code other routes/actions here
+    get "/" do
+      erb :root
+    end
+
+    get "/new" do
+      erb :"pirates/new"
+    end
+
+    post "/pirates" do
+      @pirate = Pirate.new(params[:pirate][:name], params[:pirate][:weight], params[:pirate][:height])
+      params[:pirate][:ships].each do |ship|
+        Ship.new(ship[:name],ship[:type], ship[:booty])
+      end
+      @ships = Ship.all
+      erb :"pirates/show"
+    end
 
   end
 end
--- app/models/ship.rb
+++ app/models/ship.rb
@@ -1,2 +1,21 @@
 class Ship
+attr_accessor :name, :type, :booty
+
+@@all = []
+
+def initialize(name, type, booty)
+    @name = name
+    @type = type
+    @booty = booty
+    @@all << self
+end
+
+def self.all
+    @@all
+end
+
+def self.clear
+    @@all.clear
+end
+
 end
